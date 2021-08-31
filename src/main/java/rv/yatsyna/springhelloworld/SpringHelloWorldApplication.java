39ef155a1e7fe4a868fc0bf9819fe7976f9479eb
Roman Yatsyna <alpha7animal@gmail.com>
2021-08-31 23:25:44 +0200

    Init commit.

---
+++ src/main/java/rv/yatsyna/springhelloworld/SpringHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package rv.yatsyna.springhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloWorldApplication.class, args);
+	}
+
+}
